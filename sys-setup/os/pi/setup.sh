0124845ee7a8ae99c68e9c2d831aa563093ace40
mcmhav <mcmhav@gmail.com>
2019-11-07 22:05:16 +0100

    AUTO: update system configs

--- sys-setup/os/pi/setup.sh
+++ sys-setup/os/pi/setup.sh
@@ -7,8 +7,29 @@ sudo apt-get upgrade
 
 # apt list --installed
 
+installApts() {
+  loggit "Installing apts"
+
+  while read l; do
+    read -ra APT_PACKAGE <<<$l
+    if ! dpkg -s "${APT_PACKAGE[0]}" | grep Status >/dev/null; then
+      sudo apt-get install $l
+    fi
+  done <configs/apts
+
+  loggit "Installing piApts"
+
+  while read l; do
+    read -ra APT_PACKAGE <<<$l
+    if ! dpkg -s "${APT_PACKAGE[0]}" | grep Status >/dev/null; then
+      sudo apt-get install $l
+    fi
+  done <configs/piApts
+}
+
 if [ ! -d "$NVM_DIR" ]; then
   wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+  nvm install --latest-npm
 fi
 if [ -z "$(which pyenv)" ]; then
   curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
@@ -32,22 +53,11 @@ fi
 # install packages from piApts and apts (perhaps move apts to lin)
 # apt list --installed
 
-installApts() {
-  loggit "Installing apts"
-
-  while read l; do
-    read -ra APT_PACKAGE <<<$l
-    if ! dpkg -s "${APT_PACKAGE[0]}" | grep Status >/dev/null; then
-      sudo apt-get install $l
-    fi
-  done <configs/apts
-
-  loggit "Installing piApts"
-
-  while read l; do
-    read -ra APT_PACKAGE <<<$l
-    if ! dpkg -s "${APT_PACKAGE[0]}" | grep Status >/dev/null; then
-      sudo apt-get install $l
-    fi
-  done <configs/piApts
-}
+# Docker
+sudo apt-get install apt-transport-https ca-certificates software-properties-common -y
+mkdir -p "$HOME/tmp"
+cd "$HOME/tmp"
+curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+cd "$HOME"
+sudo usermod -aG docker pi
+sudo curl https://download.docker.com/linux/raspbian/gpg
