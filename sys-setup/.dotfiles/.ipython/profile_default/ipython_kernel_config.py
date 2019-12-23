790c0f69ac4eb43bb63d45cd00d0b1b7162508b7
mcmhav <mcmhav@gmail.com>
2019-12-23 15:17:10 +0100

    add lab config?

--- sys-setup/.dotfiles/.ipython/profile_default/ipython_kernel_config.py
+++ sys-setup/.dotfiles/.ipython/profile_default/ipython_kernel_config.py
@@ -5,3 +5,6 @@ c.IPKernelApp.matplotlib = 'inline'
 c.InteractiveShellApp.exec_lines = []
 c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
 c.InteractiveShellApp.exec_lines.append('%autoreload 2')
+# c.InteractiveShellApp.exec_lines.append('%config Completer.use_jedi = False')
+# %config Completer.use_jedi = True
+c.IPCompleter.use_jedi = False
