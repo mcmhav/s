f351268626470fac30f254379289b265c19a0f84
mcmhav <mcmhav@gmail.com>
2021-01-07 17:22:49 +0100

    AUTO: update system configs

--- sys-setup/fish/config.fish
+++ sys-setup/fish/config.fish
@@ -4,15 +4,12 @@ set fish_command_timer_enabled 0
 
 export CSYS_HOME="$HOME/r/s"
 
-bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashcsysrc
-bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashHelperFuncsrc
-bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashGitrc
-bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashAliasesrc
-bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashExportsrc
-if test -e $HOME/.bashAkerrcs
-  bass source $HOME/.bashAkerrcs/.bashAkerrc
-end
-# bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashSourcerc
+bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashcsysrc fish
+# bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashHelperFuncsrc
+# bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashGitrc
+# bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashAliasesrc
+# bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashExportsrc
+bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashSourcerc
 
 # source $CSYS_HOME/sys-setup/bash/.bashrc
 
