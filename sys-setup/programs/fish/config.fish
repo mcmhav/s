62888ec3c7f7bd21fbec67b7b8d065c36ce6c437
mcmhav <mcmhav@gmail.com>
2021-06-21 14:54:31 +0200

    update config

--- sys-setup/programs/fish/config.fish
+++ sys-setup/programs/fish/config.fish
@@ -7,9 +7,8 @@ export CSYS_HOME="$HOME/r/s"
 bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashcsysrc fish
 bass source $CSYS_HOME/sys-setup/bash/bashrc/.bashSourcerc
 
-pyenv init - | source
-
 # alias pipenv='env SHELL="/usr/local/bin/fish" pipenv'
 alias :q="exit"
+status --is-login; and pyenv init --path | source
 status --is-interactive; and source (rbenv init -|psub)
 status --is-interactive; and source (nodenv init -|psub)
