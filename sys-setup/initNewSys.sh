9b0366990b16cb4d25a1baeb765a19a567a3ce2e
mcmhav <mcmhav@gmail.com>
2022-02-11 19:09:27 +0100

    fix link path

--- sys-setup/initNewSys.sh
+++ sys-setup/initNewSys.sh
@@ -12,7 +12,7 @@ non_gui_config() {
 
   ln -sf "$CSYS_HOME/sys-setup/.dotfiles/.vimrc" "$HOME/.vimrc"
   ln -sf "$CSYS_HOME/sys-setup/.dotfiles/.tmux.conf" "$HOME/.tmux.conf"
-  ln -sf "$CSYS_HOME/sys-setup/.dotfiles/zellij" "$HOME/.config/zellij"
+  ln -sf "$CSYS_HOME/sys-setup/.dotfiles/zellij" "$HOME/.config/"
   ln -sf "$CSYS_HOME/sys-setup/.dotfiles/.eslintrc.js" "$HOME/.eslintrc.js"
   ln -sf "$CSYS_HOME/sys-setup/.dotfiles/.prettierrc.js" "$HOME/.prettierrc.js"
   ln -sf "$CSYS_HOME/sys-setup/.dotfiles/.pylintrc" "$HOME/.pylintrc"
