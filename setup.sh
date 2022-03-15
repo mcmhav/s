497b784c2de8262500d322ebaf6f1118b1c17b90
mcmhav <mcmhav@gmail.com>
2022-03-15 11:49:58 +0100

    improve gitpod dotfiles compatibility

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "helloooo"
+CONFIG_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+BASHRC_LOCATION="$HOME/.bashrc.d"
+CSYS_HOME="$CONFIG_HOME"
 
-touch "$HOME/a_test_file"
+if [ ! -d "$BASHRC_LOCATION" ]; then
+  mkdir "$BASHRC_LOCATION"
+fi
+
+ln -sf "$CSYS_HOME/sys-setup/bash/.bashrc" "$BASHRC_LOCATION"
