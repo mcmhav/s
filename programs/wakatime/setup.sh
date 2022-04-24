b95872f33b1c233cc4e6cd91bc6f68cf4a2e9af1
mcmhav <mcmhav@gmail.com>
2022-04-25 01:59:18 +0200

    indet waka config

--- programs/wakatime/setup.sh
+++ programs/wakatime/setup.sh
@@ -5,14 +5,14 @@ if [ ! -d "$HOME/.wakatime" ]; then
 fi
 
 if [ ! -f "$HOME/.wakatime/.wakatime.cfg" ]; then
-  echo """[settings]
-  debug = false
-  hidefilenames = false
-  ignore =
-      COMMIT_EDITMSG$
-      PULLREQ_EDITMSG$
-      MERGE_MSG$
-      TAG_EDITMSG$
-  api_key=tmp
-  """ >>"$HOME/.wakatime/.wakatime.cfg"
+  echo "[settings]
+debug = false
+hidefilenames = false
+ignore =
+    COMMIT_EDITMSG$
+    PULLREQ_EDITMSG$
+    MERGE_MSG$
+    TAG_EDITMSG$
+api_key=tmp
+" >>"$HOME/.wakatime/.wakatime.cfg"
 fi
