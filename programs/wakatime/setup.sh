3827820b50b4c38ce058cdf56847c3b3f0ce190a
mcmhav <mcmhav@gmail.com>
2022-04-24 22:41:11 +0200

    update wakatime, add coc-sh

--- programs/wakatime/setup.sh
+++ programs/wakatime/setup.sh
@@ -3,6 +3,13 @@
 if [ ! -d "$HOME/.wakatime" ]; then
   mkdir "$HOME/.wakatime"
   echo """[settings]
+  debug = false
+  hidefilenames = false
+  ignore =
+      COMMIT_EDITMSG$
+      PULLREQ_EDITMSG$
+      MERGE_MSG$
+      TAG_EDITMSG$
   api_key=tmp
   """ >>"$HOME/.wakatime/.wakatime.cfg"
 fi
