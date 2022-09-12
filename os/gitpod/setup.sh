3f9f888923b2579e70aba32f44950964980d3532
mcmhav <mcmhav@gmail.com>
2022-09-12 16:26:17 +0200

    AUTO: update system configs

--- os/gitpod/setup.sh
+++ os/gitpod/setup.sh
@@ -4,6 +4,7 @@ SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 _setup() {
   export CSYS_LOG_LEVEL=4
+  export CSYS_SHALLOW_SETUP="true"
   loggit "Installing brew"
   csys install brew
   loggit "Installing csys-reqs"
