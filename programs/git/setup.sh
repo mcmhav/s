7e54e6c3b5871e44edc232972e19c4123de8ec30
mcmhav <mcmhav@gmail.com>
2022-09-13 16:01:01 +0200

    AUTO: update system configs

--- programs/git/setup.sh
+++ programs/git/setup.sh
@@ -4,7 +4,7 @@ PACKAGE_NAME="git"
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 _setup() {
-  if ! brew ls --version "$PACKAGE_NAME" >/dev/null; then
+  if ! csys is-package-installed "$PACKAGE_NAME"; then
     brew install "$PACKAGE_NAME"
   fi
 
