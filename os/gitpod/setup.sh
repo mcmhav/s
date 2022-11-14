e38b84e80b6a2f0797cb24daca75f4e5b0017fec
mcmhav <mcmhav@gmail.com>
2022-11-14 11:08:21 +0100

    add settings for gitpod

--- os/gitpod/setup.sh
+++ os/gitpod/setup.sh
@@ -2,6 +2,11 @@
 
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+_setup_vscode_config() {
+  loggit "Setting up vs-code-config"
+  cp "$SCRIPT_PATH/config/settings.json" "/workspace/.vscode-remote/data/Machine/"
+}
+
 _setup() {
   export CSYS_LOG_LEVEL=5
   export CSYS_SHALLOW_SETUP="true"
@@ -9,6 +14,8 @@ _setup() {
   csys install brew
   loggit "Installing csys-reqs"
   csys reqs-install "$SCRIPT_PATH/reqs"
+
+  _setup_vscode_config
 }
 
 _setup
