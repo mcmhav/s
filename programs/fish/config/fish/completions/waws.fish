59ab1a8adcc916104f92124149ba554dea39880f
mcmhav <mcmhav@gmail.com>
2023-06-16 12:59:21 +0200

    AUTO: update system configs

--- programs/fish/config/fish/completions/waws.fish
+++ programs/fish/config/fish/completions/waws.fish
@@ -1,7 +1,13 @@
+# examples:
+#  - https://github.com/fish-shell/fish-shell/tree/master/share/completions
+#  - https://fishshell.com/docs/current/completions.html
+
 set -l commands decode-message select-profile sso-profiles eks ec2 whoami
 
 complete -c waws -f
 
 complete -c waws -n "not __fish_seen_subcommand_from $commands" \
     -a "$commands"
+
+complete -c waws -s h -l help -d 'Heeeeelp'
 
