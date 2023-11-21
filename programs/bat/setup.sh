f16e4460981cf0e9017b59fd0bfbb359c8e71384
mcmhav <mcmhav@gmail.com>
2023-11-21 15:17:44 +0100

    AUTO: update system configs

---
+++ programs/bat/setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+PACKAGE_NAME="bat"
+
+_setup() {
+  case $CSYS_OS in
+    "$GITPOD_OS")
+      sudo apt-get install -y "$PACKAGE_NAME"
+      ;;
+    "$MAC_OS")
+      if ! command -v "$PACKAGE_NAME" >/dev/null; then
+        brew install "$PACKAGE_NAME"
+      fi
+      ;;
+    *)
+      echo "Unsupported OS: $CSYS_OS"
+      exit 1
+      ;;
+  esac
+}
+
+_setup
+
