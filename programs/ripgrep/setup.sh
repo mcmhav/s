046e93a2c3d5b124e6ab91e6782c781e640d563b
mcmhav <mcmhav@gmail.com>
2023-12-04 10:50:12 +0100

    AUTO: update system configs

--- programs/ripgrep/setup.sh
+++ programs/ripgrep/setup.sh
@@ -2,14 +2,17 @@
 
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 PACKAGE_NAME="ripgrep"
+PACKAGE_COMMAND="rg"
 
 _setup() {
   case $CSYS_OS in
     "$GITPOD_OS")
-      sudo apt-get install -y "$PACKAGE_NAME"
+      if ! command -v "$PACKAGE_COMMAND" >/dev/null; then
+        sudo apt-get install -y "$PACKAGE_NAME"
+      fi
       ;;
     "$MAC_OS")
-      if ! command -v "$PACKAGE_NAME" >/dev/null; then
+      if ! command -v "$PACKAGE_COMMAND" >/dev/null; then
         brew install "$PACKAGE_NAME"
       fi
       ;;
