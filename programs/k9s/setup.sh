f401aab9cc3b5127c5685bab0bea89e4efd0fd7d
mcmhav <mcmhav@gmail.com>
2024-01-24 09:15:50 +0100

    AUTO: update system configs

---
+++ programs/k9s/setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+PACKAGE_NAME="k9s"
+VERSION="v0.31.7"
+
+_intall() {
+  case $CSYS_OS in
+    "$LIN_OS"*)
+      curl -fsSL https://github.com/derailed/k9s/releases/download/$VERSION/k9s_Freebsd_amd64.tar.gz | tar -xz -C /tmp
+      sudo mv /tmp/k9s /usr/local/bin
+      ;;
+    "$MAC_OS")
+      brew install "$PACKAGE_NAME"
+      ;;
+    *)
+      echo "Unsupported OS: $CSYS_OS"
+      exit 1
+      ;;
+  esac
+}
+
+_setup() {
+  if ! command -v "$PACKAGE_NAME" >/dev/null; then
+    _install
+  fi
+}
+
+_setup
