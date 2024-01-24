0809420655d93fac980c27ba839d50c8c7205f1c
mcmhav <mcmhav@gmail.com>
2024-01-24 10:18:22 +0100

    AUTO: update system configs

--- programs/fish/config/fish/config.fish
+++ programs/fish/config/fish/config.fish
@@ -4,7 +4,7 @@ set fish_greeting
 set fish_command_timer_enabled 0
 
 if set -q -g csys_fish_up
-  echo "we should be up"
+  # echo "we should be up"
   bass source $CSYS_HOME/programs/bash/bashrc.d/aliases.bashrc
   bass source $CSYS_HOME/programs/bash/bashrc.d/git.bashrc
   set --global --export csys_fish_short_circut_setup 1
@@ -15,12 +15,9 @@ set --global --export csys_fish_short_circut_setup 0
 set SCRIPT_LOCATION (readlink (dirname (status --current-filename)))
 set --export CSYS_HOME (realpath "$SCRIPT_LOCATION/../../../..")
 
-echo "sourcing rc files"
 test -d "$HOME/.bashrc.d"; and for i in "$HOME/.bashrc.d/"*; bass source "$i"; end
-echo "done sourcing rc files"
 bass source $CSYS_HOME/programs/bash/bashrc.d/csys.bashrc fish
 bass source $CSYS_HOME/programs/bash/bashrc.d/sourcer.bashrc
-echo "done sourcing csys.bashrc"
 
 # TODO: Should be set earlier for usage in other scripts
 set --export SHELL (which fish)
@@ -28,6 +25,5 @@ set --export SHELL (which fish)
 status --is-interactive; and type -q pyenv; and source (pyenv init -|psub)
 status --is-interactive; and type -q rbenv; and source (rbenv init -|psub)
 status --is-interactive; and type -q nodenv; and source (nodenv init -|psub)
-echo "done sourcing envs"
 
 set --global --export csys_fish_up 1
