a373333cb1cb1994dee1d90e892ace8f2d8cd0a3
mcmhav <mcmhav@gmail.com>
2024-06-11 12:48:56 +0200

    AUTO: update system configs

--- programs/brew/setup.sh
+++ programs/brew/setup.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/env bash
 
 _setup() {
-  if ! command -v brew >/dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    if [ "$?" != 0 ]; then
-      loggit errr "Could not install brew. Exiting"
-      exit 1
-    fi
-  fi
+	if ! command -v brew >/dev/null; then
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		if [ "$?" != 0 ]; then
+			loggit errr "Could not install brew. Exiting"
+			exit 1
+		fi
+	fi
 
-  BREW_TAPS=$(brew tap)
-  if ! echo "$BREW_TAPS" | grep -q 'homebrew/cask-fonts'; then
-    brew tap homebrew/cask-fonts &
-  fi
-  if ! echo "$BREW_TAPS" | grep -q 'homebrew/services'; then
-    brew tap homebrew/services &
-  fi
+	BREW_TAPS=$(brew tap)
+	if ! echo "$BREW_TAPS" | grep -q 'homebrew/services'; then
+		brew tap homebrew/services
+	fi
 
-  if [ "$CSYS_SHALLOW_SETUP" != "true" ]; then
-    brew update
-    brew upgrade
-  fi
+	if [ "$CSYS_SHALLOW_SETUP" != "true" ]; then
+		brew update
+		brew upgrade
+	fi
 }
 
 _setup
