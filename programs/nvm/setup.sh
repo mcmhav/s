b5c505e36d5125e15997240cb76a9d43ea682537
mcmhav <mcmhav@gmail.com>
2024-06-11 14:59:27 +0200

    AUTO: update system configs

--- programs/nvm/setup.sh
+++ programs/nvm/setup.sh
@@ -2,31 +2,31 @@
 
 PACKAGE_NAME="nvm"
 DESIRED_NODE_VERSION="18.16.0"
-NVM_VERSION="0.39.3"
+NVM_VERSION="0.39.7"
 
 _with_nvm() {
-  export NVM_DIR="$HOME/.nvm"
-  if [ ! -d "$NVM_DIR" ] || [ ! -f "$NVM_DIR/nvm.sh" ]; then
-    # brew install "$PACKAGE_NAME"
-    mkdir -p "$NVM_DIR"
-    cd "$NVM_DIR" || exit
-    curl -fsSL "https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh" | PROFILE=/dev/null bash
-  fi
-  source "$NVM_DIR/nvm.sh"
-  if ! nvm ls --no-alias | grep -q "$DESIRED_NODE_VERSION"; then
-    nvm install --lts "$DESIRED_NODE_VERSION"
-    nvm alias default "$DESIRED_NODE_VERSION"
-  fi
+	export NVM_DIR="$HOME/.nvm"
+	if [ ! -d "$NVM_DIR" ] || [ ! -f "$NVM_DIR/nvm.sh" ]; then
+		# brew install "$PACKAGE_NAME"
+		mkdir -p "$NVM_DIR"
+		cd "$NVM_DIR" || exit
+		curl -fsSL "https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh" | PROFILE=/dev/null bash
+	fi
+	source "$NVM_DIR/nvm.sh"
+	if ! nvm ls --no-alias | grep -q "$DESIRED_NODE_VERSION"; then
+		nvm install --lts "$DESIRED_NODE_VERSION"
+		nvm alias default "$DESIRED_NODE_VERSION"
+	fi
 }
 
 _setup() {
-  case "$CSYS_OS" in
-  "$LIN_OS"*) _with_nvm ;;
-  "$MAC_OS") _with_nvm ;;
-  *)
-    loggit warn "$PACKAGE_NAME install not implemented for OS: $CSYS_OS"
-    ;;
-  esac
+	case "$CSYS_OS" in
+	"$LIN_OS"*) _with_nvm ;;
+	"$MAC_OS") _with_nvm ;;
+	*)
+		loggit warn "$PACKAGE_NAME install not implemented for OS: $CSYS_OS"
+		;;
+	esac
 }
 
 _setup
