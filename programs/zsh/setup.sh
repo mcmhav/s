857b3916c2378180a2e3dd7005e68cbb63292340
mcmhav <mcmhav@gmail.com>
2024-10-10 09:39:12 +0200

    AUTO: update system configs

--- programs/zsh/setup.sh
+++ programs/zsh/setup.sh
@@ -2,6 +2,7 @@
 
 PACKAGE_NAME="zsh"
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+ZSH_CONFIG_HOME="$HOME/.config/zsh"
 
 _setup() {
 	if ! command -v "$PACKAGE_NAME" >/dev/null; then
@@ -12,6 +13,11 @@ _setup() {
 		rm -rf "$HOME/.zshrc"
 	fi
 	ln -sf "$SCRIPT_PATH/config/.zshrc" "$HOME/.zshrc"
+	ln -sf "$SCRIPT_PATH/config/zsh" "$ZSH_CONFIG_HOME"
+
+	if ! test -d "$ZSH_CONFIG_HOME/zsh-autosuggestions"; then
+		git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CONFIG_HOME/zsh-autosuggestions"
+	fi
 }
 
 _setup
