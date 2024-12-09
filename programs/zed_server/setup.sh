e7b10b8c7bfa74391796eaecf562f75408f608cf
mcmhav <mcmhav@gmail.com>
2024-12-09 13:46:45 +0100

    AUTO: update system configs

--- programs/zed_server/setup.sh
+++ programs/zed_server/setup.sh
@@ -6,7 +6,7 @@ _install() {
 	tmp="$(mktemp -d "/tmp/zed-XXXXXX")"
 
 	# https://zed.dev/docs/remote-development?highlight=ssh#initializing-the-remote-server
-	curl -L "https://github.com/zed-industries/zed/releases/download/v$ZED_SERVER_VERSION/zed-remote-server-linux-x86_64.gz" \
+	curl -fsSL "https://github.com/zed-industries/zed/releases/download/v$ZED_SERVER_VERSION/zed-remote-server-linux-x86_64.gz" \
 		-o "$tmp/zed-remote-server-stable-linux-x86_64.gz"
 
 	gzip -d "$tmp/zed-remote-server-stable-linux-x86_64.gz"
