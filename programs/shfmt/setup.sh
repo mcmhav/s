b951b4af54aa27a9ff3aebc9580b647651a845ba
mcmhav <mcmhav@gmail.com>
2024-12-09 13:27:25 +0100

    AUTO: update system configs

---
+++ programs/shfmt/setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+PACKAGE_NAME="shfmt"
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+VERSION="3.10.0"
+
+_install() {
+	case $CSYS_OS in
+	"$LIN_OS"*)
+		curl -fsSL "https://github.com/mvdan/sh/releases/download/v$VERSION/shfmt_v${VERSION}_linux_amd64" -o /tmp/shfmt
+		sudo mv /tmp/shfmt /usr/local/bin
+		;;
+	"$MAC_OS")
+		brew install "$PACKAGE_NAME"
+		;;
+	*)
+		echo "Unsupported OS: $CSYS_OS"
+		exit 1
+		;;
+	esac
+}
+_setup() {
+	if ! command -v "$PACKAGE_NAME" >/dev/null; then
+		_install
+	fi
+}
+
+_setup
