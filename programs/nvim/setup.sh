2a58d2a720e40ec7292464135541359d18f9702f
mcmhav <mcmhav@gmail.com>
2025-02-11 09:39:23 +0100

    AUTO: update system configs

--- programs/nvim/setup.sh
+++ programs/nvim/setup.sh
@@ -1,26 +1,39 @@
 #!/usr/bin/env bash
 
 PACKAGE_NAME="nvim"
+NVIM_VERSION="0.10.4"
+
+_install_from_tar() {
+    NVIM_HOME="$HOME/.local/nvim"
+    mkdir -p "$NVIM_HOME"
+    curl -fsSL "https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-linux-x86_64.tar.gz" | tar -xz -C "$NVIM_HOME"
+    ln -sf "$NVIM_HOME/nvim-linux-x86_64/bin/nvim" "$HOME/.local/bin/nvim"
+}
 
 _setup() {
-  case $CSYS_OS in
-    "$GITPOD_OS")
-      if ! command -v "$PACKAGE_NAME" >/dev/null; then
-        sudo apt-get install -y neovim
-      fi
-      ;;
-    "$MAC_OS")
-      if ! command -v "$PACKAGE_NAME" >/dev/null; then
-        brew install "$PACKAGE_NAME"
-      fi
-      ;;
-    *)
-      echo "Unsupported OS: $CSYS_OS"
-      exit 1
-      ;;
-  esac
+    case $CSYS_OS in
+        "$GITPOD_OS")
+            if ! command -v "$PACKAGE_NAME" >/dev/null; then
+                sudo apt-get install -y neovim
+            else
+                CURRENT_VERSION=$(nvim --version | head -n 1 | awk '{print $2}')
+                if ! echo -e "v$NVIM_VERSION\n$CURRENT_VERSION" | sort -V -C; then
+                    _install_from_tar
+                fi
+            fi
+            ;;
+        "$MAC_OS")
+            if ! command -v "$PACKAGE_NAME" >/dev/null; then
+                brew install "$PACKAGE_NAME"
+            fi
+            ;;
+        *)
+            echo "Unsupported OS: $CSYS_OS"
+            exit 1
+            ;;
+    esac
 
-  ln -sf "$SCRIPT_PATH/config/nvim" "$HOME/.config"
+    ln -sf "$SCRIPT_PATH/config/nvim" "$HOME/.config"
 }
 
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
--- programs/zed/setup.sh
+++ programs/zed/setup.sh
@@ -1,28 +1,37 @@
 #!/usr/bin/env bash
 
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-ZED_VERSION="0.169.2"
+ZED_APP_PATH="$HOME/.local/zed/Applications"
+ZED_VERSION="0.170.2"
 
 _install() {
-	# ref. https://zed.dev/install.sh
-	tmp="$(mktemp -d "/tmp/zed-XXXXXX")"
-	curl -sL "https://github.com/zed-industries/zed/releases/download/v$ZED_VERSION/Zed-x86_64.dmg" -o "$tmp/Zed.dmg"
-	hdiutil attach -quiet "$tmp/Zed.dmg" -mountpoint "$tmp/mount"
+    # ref. https://zed.dev/install.sh
+    tmp="$(mktemp -d "/tmp/zed-XXXXXX")"
+    curl -sL "https://github.com/zed-industries/zed/releases/download/v$ZED_VERSION/Zed-x86_64.dmg" -o "$tmp/Zed.dmg"
+    hdiutil attach -quiet "$tmp/Zed.dmg" -mountpoint "$tmp/mount"
 
-	mkdir -p "$HOME/.local/zed/Applications"
-	ditto "$tmp/mount/Zed.app" "$HOME/.local/zed/Applications/Zed.app"
-	hdiutil detach -quiet "$tmp/mount"
+    mkdir -p "$ZED_APP_PATH"
+    ditto "$tmp/mount/Zed.app" "$HOME/.local/zed/Applications/Zed.app"
+    hdiutil detach -quiet "$tmp/mount"
 
-	ln -sf "$HOME/.local/zed/Applications/Zed.app/Contents/MacOS/cli" "$HOME/.local/bin/zed"
+    mkdir -p "$HOME/.local/bin"
+    ln -sf "$HOME/.local/zed/Applications/Zed.app/Contents/MacOS/cli" "$HOME/.local/bin/zed"
 }
 
 _setup() {
-	if ! command -v zed &>/dev/null; then
-		_install
-	fi
+    if ! command -v zed &>/dev/null; then
+        _install
+    else
+        CURRENT_ZED_VERSION="$(zed --version | awk '{print $2}')"
+        if ! echo -e "$ZED_VERSION\n$CURRENT_ZED_VERSION" | sort -V -C; then
+            rm -rf "$ZED_APP_PATH"
+            _install
+        fi
+    fi
 
-	ln -sf "$SCRIPT_PATH/config/keymap.json" "$HOME/.config/zed/"
-	ln -sf "$SCRIPT_PATH/config/settings.json" "$HOME/.config/zed/"
+    mkdir -p "$HOME/.config/zed"
+    ln -sf "$SCRIPT_PATH/config/keymap.json" "$HOME/.config/zed/"
+    ln -sf "$SCRIPT_PATH/config/settings.json" "$HOME/.config/zed/"
 }
 
 _setup
