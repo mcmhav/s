f5af3db7073c4f7bb8f195b32e4ccf029a59995b
mcmhav <mcmhav@gmail.com>
2025-03-04 11:11:04 +0100

    AUTO: update system configs

--- os/lin/setup.sh
+++ os/lin/setup.sh
@@ -3,8 +3,8 @@
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 _setup() {
-  loggit "Installing brew"
-  csys install brew
+  # loggit "Installing brew"
+  # csys install brew
   loggit "Installing csys-reqs"
   csys reqs-install "$SCRIPT_PATH/reqs"
 }
