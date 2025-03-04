f50548b0404ef59eeb920671a3af65e6281e4d50
mcmhav <mcmhav@gmail.com>
2025-03-04 12:45:50 +0100

    AUTO: update system configs

--- programs/fish/setup.sh
+++ programs/fish/setup.sh
@@ -4,9 +4,16 @@ PACKAGE_NAME="fish"
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 _setup() {
-	if ! command -v "$PACKAGE_NAME" >/dev/null; then
-		brew install "$PACKAGE_NAME"
-	fi
+    case $CSYS_OS in
+        "$GITPOD_OS"* | "$MAC_OS")
+            if ! command -v "$PACKAGE_NAME" >/dev/null; then
+                brew install "$PACKAGE_NAME"
+            fi
+            ;;
+        *)
+            loggit warn "Unsupported OS: $CSYS_OS"
+            ;;
+    esac
 
 	if ! test -L "$HOME/.config/fish"; then
 		rm -rf "$HOME/.config/fish"
