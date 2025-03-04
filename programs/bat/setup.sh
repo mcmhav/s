04265dd50554ff45a334cf94e852a1b8d978b55d
mcmhav <mcmhav@gmail.com>
2025-03-04 10:42:25 +0100

    AUTO: update system configs

--- programs/bat/setup.sh
+++ programs/bat/setup.sh
@@ -6,9 +6,10 @@ _VERSION="0.24.0"
 
 _install() {
 	case $CSYS_OS in
-	"$GITPOD_OS")
-		curl -fsSL "https://github.com/sharkdp/bat/releases/download/v$_VERSION/bat-v$_VERSION-x86_64-unknown-linux-musl.tar.gz" | tar -xz -C /tmp
-		cp "/tmp/bat-v$_VERSION-x86_64-unknown-linux-musl/bat" ~/.local/bin
+    "$LIN_OS"*)
+        ARCH=$(uname -m)
+		curl -fsSL "https://github.com/sharkdp/bat/releases/download/v$_VERSION/bat-v$_VERSION-${ARCH}-unknown-linux-musl.tar.gz" | tar -xz -C /tmp
+		cp "/tmp/bat-v$_VERSION-${ARCH}-unknown-linux-musl/bat" ~/.local/bin
 		;;
 	"$MAC_OS")
 		brew install "$PACKAGE_NAME"
