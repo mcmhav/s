fb0106d09ece7ea950d7d9f51b98a43f0f263a3e
mcmhav <mcmhav@gmail.com>
2025-03-25 09:11:51 +0100

    AUTO: update system configs

--- programs/kubectl/setup.sh
+++ programs/kubectl/setup.sh
@@ -3,15 +3,28 @@
 PACKAGE_COMMAND="kubectl"
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
+_install() {
+    case $CSYS_OS in
+    "$LIN_OS"*)
+        RELEASE_VERSION="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
+        curl \
+            -fsSL "https://storage.googleapis.com/kubernetes-release/release/$RELEASE_VERSION/bin/linux/amd64/kubectl" \
+            -o /usr/local/bin/kubectl
+        chmod +x /usr/local/bin/kubectl
+        ;;
+    "$MAC_OS")
+        brew install "$PACKAGE_COMMAND"
+        ;;
+    *)
+        echo "Unsupported OS: $CSYS_OS, pgk: $PACKAGE_COMMAND"
+        exit 1
+        ;;
+    esac
+}
 _setup() {
-  if ! command -v "$PACKAGE_COMMAND" >/dev/null; then
-    # RELEASE_VERSION="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
-    # curl \
-    #  -fsSL "https://storage.googleapis.com/kubernetes-release/release/$RELEASE_VERSION/bin/linux/amd64/kubectl" \
-    #  -o /usr/local/bin/kubectl
-    # chmod +x /usr/local/bin/kubectl
-    brew install kubectl
-  fi
+    if ! command -v "$PACKAGE_COMMAND" >/dev/null; then
+        _install
+    fi
 }
 
 _setup
