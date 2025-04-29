a3e00b21360af09403a38e5ca8d1326846f236f4
mcmhav <mcmhav@gmail.com>
2025-04-29 13:04:34 +0200

    AUTO: update system configs

--- programs/zed_server/setup.sh
+++ programs/zed_server/setup.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-ZED_SERVER_VERSION="0.177.7"
+ZED_SERVER_VERSION="0.183.11"
 
 _install() {
-	tmp="$(mktemp -d "/tmp/zed-XXXXXX")"
+    tmp="$(mktemp -d "/tmp/zed-XXXXXX")"
 
-	# https://zed.dev/docs/remote-development?highlight=ssh#initializing-the-remote-server
-	curl -fsSL "https://github.com/zed-industries/zed/releases/download/v$ZED_SERVER_VERSION/zed-remote-server-linux-x86_64.gz" \
-		-o "$tmp/zed-remote-server-stable-linux-x86_64.gz"
+    # https://zed.dev/docs/remote-development?highlight=ssh#initializing-the-remote-server
+    curl -fsSL "https://github.com/zed-industries/zed/releases/download/v$ZED_SERVER_VERSION/zed-remote-server-linux-x86_64.gz" \
+        -o "$tmp/zed-remote-server-stable-linux-x86_64.gz"
 
-	gzip -d "$tmp/zed-remote-server-stable-linux-x86_64.gz"
-	chmod +x "$tmp/zed-remote-server-stable-linux-x86_64"
-	mkdir -p "$HOME/.zed_server"
-	cp "$tmp/zed-remote-server-stable-linux-x86_64" "$HOME/.zed_server"
+    gzip -d "$tmp/zed-remote-server-stable-linux-x86_64.gz"
+    chmod +x "$tmp/zed-remote-server-stable-linux-x86_64"
+    mkdir -p "$HOME/.zed_server"
+    cp "$tmp/zed-remote-server-stable-linux-x86_64" "$HOME/.zed_server"
 }
 
 _setup() {
-	# https://zed.dev/docs/remote-development?highlight=ssh#initializing-the-remote-server
-	if [ ! -f "$HOME/.zed_server/zed-remote-server-stable-linux-x86_64" ]; then
-		_install
-	fi
+    # https://zed.dev/docs/remote-development?highlight=ssh#initializing-the-remote-server
+    if [ ! -f "$HOME/.zed_server/zed-remote-server-stable-linux-x86_64" ]; then
+        _install
+    fi
 }
 
 _setup
