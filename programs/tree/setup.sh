f7c1adf7eae4aa1393748c5d28dd6f0ba121f798
mcmhav <mcmhav@gmail.com>
2025-05-07 09:06:09 +0200

    AUTO: update system configs

--- programs/tree/setup.sh
+++ programs/tree/setup.sh
@@ -4,31 +4,32 @@ SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 PACKAGE_NAME="tree"
 
 _install() {
-	case $CSYS_OS in
+    case $CSYS_OS in
     "$LIN_OS"*)
         if command -v yum >/dev/null; then
             sudo yum install -y "$PACKAGE_NAME"
         elif command -v apt-get >/dev/null; then
+            sudo apt-get update
             sudo apt-get install -y "$PACKAGE_NAME"
         else
             echo "Package manager not found"
             exit 1
         fi
-		;;
-	"$MAC_OS")
-		brew install "$PACKAGE_NAME"
-		;;
-	*)
-		echo "Unsupported OS: $CSYS_OS, pgk: $PACKAGE_NAME"
-		exit 1
-		;;
-	esac
+        ;;
+    "$MAC_OS")
+        brew install "$PACKAGE_NAME"
+        ;;
+    *)
+        echo "Unsupported OS: $CSYS_OS, pgk: $PACKAGE_NAME"
+        exit 1
+        ;;
+    esac
 }
 
 _setup() {
-	if ! command -v "$PACKAGE_NAME" >/dev/null; then
-		_install
-	fi
+    if ! command -v "$PACKAGE_NAME" >/dev/null; then
+        _install
+    fi
 }
 
 _setup
