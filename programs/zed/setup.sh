a89f9f62184285a2117dd94089a0bd318ac91c22
mcmhav <mcmhav@gmail.com>
2025-09-12 10:35:00 +0200

    AUTO: update system configs

--- programs/zed/setup.sh
+++ programs/zed/setup.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ZED_APP_PATH="$HOME/.local/zed/Applications"
-ZED_VERSION="0.201.5"
+ZED_VERSION="0.203.4"
 
 _install() {
     # ref. https://zed.dev/install.sh
